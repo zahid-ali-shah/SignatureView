2130f6b901536f81f9ba4e3d0d1eb47a5559984c
Syed Zahid <syed.zahid.ali.sha@gmail.com>
2015-09-20 15:31:06 +0500

    Pushing complete code

---
+++ signatureview/src/main/java/com/kyanogen/signatureview/model/Point.java
@@ -0,0 +1,24 @@
+package com.kyanogen.signatureview.model;
+
+/**
+ * Created by Zahid.Ali on 3/24/2015.
+ */
+public class Point {
+    public final float x;
+    public final float y;
+    public final long time;
+
+    public Point(float x, float y, long time){
+        this.x = x;
+        this.y = y;
+        this.time = time;
+    }
+
+    private float distanceTo(Point start){
+        return (float) (Math.sqrt(Math.pow((x - start.x), 2) + Math.pow((y - start.y), 2)));
+    }
+
+    public float velocityFrom(Point start) {
+        return distanceTo(start) / (this.time - start.time);
+    }
+}
